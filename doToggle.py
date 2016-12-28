8ffc32219c7e9b4e9a9b315701469a4c411d4dbb
gtetil <garrett.tetil@gmail.com>
2016-12-28 17:11:29 -0500

    finished switching over to kivy format.

--- doToggle.py
+++ doToggle.py
@@ -9,11 +9,17 @@ Builder.load_string("""
     font_size: '20sp'
     size_hint: None, None
     size: 153.33, 153.33
+    ignition_input: app.arduino.ignition_input
     on_press: app.arduino.set('digital/' + str(self.index) + '/', self.state)
 
 """)
 
 class DOToggle(ToggleButton):
     index = NumericProperty(0)
+    ignition_input = NumericProperty(0)
+
+    def on_ignition_input(self, instance, state):
+        if state == 0:
+            self.state = 'normal'
 
 Factory.register('KivyB', module='DOToggle')
--- main.py
+++ main.py
@@ -7,13 +7,16 @@ from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.clock import Clock
-from kivy.properties import StringProperty, NumericProperty
+from kivy.properties import StringProperty, NumericProperty, ObjectProperty
 from kivy.uix.camera import Camera
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
-
+from kivy.uix.popup import Popup
+from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.config import Config
 
 Window.size = (800,480)
+Config.set('graphics', 'maxfps', '100')
 
 import diIndicator, doToggle
 import serial
@@ -21,36 +24,80 @@ import os
 BASE = "/sys/class/backlight/rpi_backlight/"
 
 debug_mode = 0
-cam = Camera(resolution=(800, 480))
 
-class MainScreen(Screen):
-    pass
-
-class MainApp(App):
+class ScreenManagement(ScreenManager):
     passcode = '1234'
     passcode_try = ''
-    logged_in = NumericProperty(0)
+    logged_in = 0
+    ignition_input = NumericProperty(0)
+    reverse_input = NumericProperty(0)
+    passcode_ref = ObjectProperty(None)
 
-    def build(self):
-        self.arduino = Arduino()
-        return MainScreen()
+    def __init__(self,**kwargs):
+        super (ScreenManagement,self).__init__(**kwargs)
+        self.transition = NoTransition()
+
+    def on_ignition_input(self, instance, state):
+        if state == 1:
+            self.current = 'passcode_screen'
+            state_str = '0'
+        else:
+            self.logged_in = 0
+            state_str = '1'
+        #_power = open(os.path.join(BASE, "bl_power"), "w")
+        #_power.write(state_str)
+        #_power.close()
+
+    def on_reverse_input(self, instance, state):
+        if state == 1:
+            self.current = "camera_screen"
+        else:
+            if self.logged_in == 1:
+                self.current = "main_screen"
+            else:
+                self.current = "passcode_screen"
 
     def try_passcode(self, number):
         self.passcode_try = self.passcode_try + number
         if len(self.passcode_try) == len(self.passcode):
             if self.passcode_try == self.passcode:
+                self.current = 'main_screen'
                 self.logged_in = 1
-                self.root.add_widget(self.root.ids.logout_button)
             else:
-                self.root.ids.main_tabbed_panel.switch_to(self.root.ids.main)
+                self.current_screen.ids.fail_popup.open()
             self.passcode_try = ''
-            self.root.ids.passcode_popup.dismiss()
+
+class MainScreen(Screen):
+    pass
+
+class PasscodeScreen(Screen):
+    pass
+
+class CameraScreen(Screen):
+    reverse_input = NumericProperty(0)
+
+    def __init__(self,**kwargs):
+        super (CameraScreen,self).__init__(**kwargs)
+        self.cam = Camera(resolution=(800, 480))
+        self.add_widget(self.cam)
+
+    def on_reverse_input(self, instance, state):
+        if state == 1:
+            self.cam.play = True
+        else:
+            self.cam.play = False
+
+class MainApp(App):
+
+    def build(self):
+        self.arduino = Arduino()
+        return ScreenManagement()
 
     def brightness(self, value):
         if value > 0 and value < 256:
-            _brightness = open(os.path.join(BASE, "brightness"), "w")
-            _brightness.write(str(value))
-            _brightness.close()
+            #_brightness = open(os.path.join(BASE, "brightness"), "w")
+            #_brightness.write(str(value))
+            #_brightness.close()
             return
         raise TypeError("Brightness should be between 0 and 255")
 
@@ -59,6 +106,8 @@ class MainApp(App):
 
 class Arduino(Widget):
     digital_inputs = NumericProperty(0)
+    reverse_input = NumericProperty(0)
+    ignition_input = NumericProperty(0)
 
     def __init__(self, **kwargs):
         super(Arduino, self).__init__(**kwargs)
@@ -71,6 +120,8 @@ class Arduino(Widget):
                 self.digital_inputs = 0
                 try:
                     self.digital_inputs = int(serial_data)
+                    self.reverse_input = self.digital_inputs & 1
+                    self.ignition_input = (self.digital_inputs & 1000000) >> 6
                 except ValueError:
                     print('value error')
             except:
