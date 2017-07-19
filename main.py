644537b921ad40583cf322dbd99036f623f0dc44
titetil <gtetil@us.tiauto.com>
2017-07-19 12:39:01 -0400

    - integrated scatter widgets into main application, and debugged.

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ from kivy.animation import Animation
 from navigationdrawer import NavigationDrawer
 from kivy.uix.settings import SettingsWithSidebar, SettingString, SettingNumeric, SettingSpacer
 from kivy.metrics import dp
+from kivy.uix.scatterlayout import ScatterLayout
+from kivy.uix.scatter import Scatter
+from kivy.graphics.transformation import Matrix
 import app_settings
 
 import json
@@ -152,7 +155,8 @@ class DynamicLayout(Widget):
 
     def save_layout(self):
         json_data = []
-        for dyn_widget in self.main_layout.children:
+        for scatter_layout in self.main_layout.children:
+            dyn_widget = scatter_layout.children[0].children[0]
             data = {}
             data['id'] = dyn_widget.id
             data['label'] = dyn_widget.label
@@ -161,8 +165,8 @@ class DynamicLayout(Widget):
             data['var_alias'] = dyn_widget.var_alias
             data['enable'] = dyn_widget.enable
             data['invert'] = dyn_widget.invert
-            data['pos'] = dyn_widget.pos
-            data['size'] = dyn_widget.size
+            data['pos'] = scatter_layout.pos
+            data['size'] = scatter_layout.size
             json_data.append(data)
         with open(self.layout_file, 'w') as file:
             json.dump(json_data, file, sort_keys=True, indent=4)
@@ -200,29 +204,33 @@ class DynamicLayout(Widget):
             else:
                 item_label = label
             if toggle:
-                dyn_widget = DynToggleButton(text=item_label, id=str(id), pos=pos, size_hint=(None, None), height=size[1], width=size[0])
+                dyn_widget = DynToggleButton(text=item_label, id=str(id))
             else:
-                dyn_widget = DynButton(text=item_label, id=str(id), pos=pos, size_hint=(None, None), height=size[1], width=size[0])
+                dyn_widget = DynButton(text=item_label, id=str(id))
+            scatter_layout = MyScatterLayout(id=str(id) + '_scatter', do_rotation=False, size=(size[0], size[1]), size_hint=(None, None), pos=pos)
             dyn_widget.set_properties(self, self.var_alias, self.var_tag, enable, self.item_edit_popup, invert, label)
-            self.main_layout.add_widget(dyn_widget)
+            scatter_layout.add_widget(dyn_widget)
+            self.main_layout.add_widget(scatter_layout)
         self.app_ref.variables.refresh_data = True
         if self.modify_mode:
             self.modify_screen()
 
     def modify_screen(self):
         self.modify_mode = True
-        for dyn_widget in self.main_layout.children:
-            self.animate(dyn_widget)
+        for scatter_layout in self.main_layout.children:
+            self.animate(scatter_layout.children[0].children[0])
 
     def end_modify(self):
         self.modify_mode = False
-        for dyn_widget in self.main_layout.children:
+        for scatter_layout in self.main_layout.children:
+            dyn_widget = scatter_layout.children[0].children[0]
             Animation.cancel_all(dyn_widget)
             dyn_widget.background_color = 1, 1, 1, 1
+        self.save_layout() #save for size and position changes
 
     def animate(self, widget):
-        anim = Animation(background_color=[1, 1, 0, 0.75], duration=0.5, t='linear') + Animation(
-            background_color=[1, 1, 1, 1], duration=0.5, t='linear')
+        anim = Animation(background_color=[1, 1, 0, 0.75], duration=1, t='linear') + Animation(
+            background_color=[1, 1, 1, 1], duration=1, t='linear')
         anim.repeat = True
         anim.start(widget)
 
@@ -304,15 +312,17 @@ class DynItem(Widget):
         if self.collide_point(*touch.pos):
             if self.app_ref.slide_layout.state != 'open':
                 if not self.dynamic_layout.modify_mode:
-                    if self.enable and self.var_alias != 'SYS_LOGGED_IN': #don't allow SYS_LOGGED_IN to be change by a button
-                        self._do_press()
-                        self.output_cmd()
+                    if self.enable:
+                        if self.var_alias != 'SYS_LOGGED_IN': #don't allow SYS_LOGGED_IN to be change by a button
+                            self._do_press()
+                            self.output_cmd()
                         return xor(True, self.invert)
                     else:
                         return xor(False, self.invert)
                 else:
-                    self.item_edit_popup.edit_popup(self.item_edit_popup, self, False)
-                    return False
+                    if touch.is_double_tap:
+                        self.item_edit_popup.edit_popup(self.item_edit_popup, self, False)
+                        return False
             return False
         return super(DynItem, self).on_touch_down(touch)
 
@@ -320,15 +330,17 @@ class DynItem(Widget):
         if self.collide_point(*touch.pos):
             if self.app_ref.slide_layout.state != 'open':
                 if not self.dynamic_layout.modify_mode and not self.toggle:
-                    if self.enable and self.var_alias != 'SYS_LOGGED_IN': #don't allow SYS_LOGGED_IN to be change by a button
-                        self._do_release()
-                        self.output_cmd()
+                    if self.enable:
+                        if self.var_alias != 'SYS_LOGGED_IN':  # don't allow SYS_LOGGED_IN to be change by a button
+                            self._do_release()
+                            self.output_cmd()
                         return True
                     else:
                         return False
                 else:
-                    self.item_edit_popup.edit_popup(self.item_edit_popup, self, False)
-                    return False
+                    if touch.is_double_tap:
+                        self.item_edit_popup.edit_popup(self.item_edit_popup, self, False)
+                        return False
             return False
         return super(DynItem, self).on_touch_down(touch)
 
@@ -793,6 +805,133 @@ class Variables(Widget):
                 _brightness.close()
                 return
             #raise TypeError("Brightness should be between 0 and 255")
+
+class MyScatterLayout(ScatterLayout):
+    move_lock = False
+    scale_lock_left = False
+    scale_lock_right = False
+    scale_lock_top = False
+    scale_lock_bottom = False
+
+    def on_touch_up(self, touch):
+        self.move_lock = False
+        self.scale_lock_left = False
+        self.scale_lock_right = False
+        self.scale_lock_top = False
+        self.scale_lock_bottom = False
+        if touch.grab_current is self:
+            touch.ungrab(self)
+            x = self.pos[0] / 10
+            x = round(x, 0)
+            x = x * 10
+            y = self.pos[1] / 10
+            y = round(y, 0)
+            y = y * 10
+            self.pos = x, y
+        return super(MyScatterLayout, self).on_touch_up(touch)
+
+    def transform_with_touch(self, touch):
+        changed = False
+        x = self.bbox[0][0]
+        y = self.bbox[0][1]
+        width = self.bbox[1][0]
+        height = self.bbox[1][1]
+        mid_x = x + width / 2
+        mid_y = y + height / 2
+        inner_width = width * 0.5
+        inner_height = height * 0.5
+        left = mid_x - (inner_width / 2)
+        right = mid_x + (inner_width / 2)
+        top = mid_y + (inner_height / 2)
+        bottom = mid_y - (inner_height / 2)
+
+            # just do a simple one finger drag
+        if len(self._touches) == self.translation_touches:
+            # _last_touch_pos has last pos in correct parent space,
+            # just like incoming touch
+            dx = (touch.x - self._last_touch_pos[touch][0]) \
+                 * self.do_translation_x
+            dy = (touch.y - self._last_touch_pos[touch][1]) \
+                 * self.do_translation_y
+            dx = dx / self.translation_touches
+            dy = dy / self.translation_touches
+            if (touch.x > left and touch.x < right and touch.y < top and touch.y > bottom or self.move_lock) and not self.scale_lock_left and not self.scale_lock_right and not self.scale_lock_top and not self.scale_lock_bottom:
+                self.move_lock = True
+                self.apply_transform(Matrix().translate(dx, dy, 0))
+                changed = True
+
+        change_x = touch.x - self.prev_x
+        change_y = touch.y - self.prev_y
+        anchor_sign = 1
+        sign = 1
+        if abs(change_x) >= 9 and not self.move_lock and not self.scale_lock_top and not self.scale_lock_bottom:
+            if change_x < 0:
+                sign = -1
+            if (touch.x < left or self.scale_lock_left) and not self.scale_lock_right:
+                self.scale_lock_left = True
+                self.pos = (self.pos[0] + (sign * 10), self.pos[1])
+                anchor_sign = -1
+            elif (touch.x > right or self.scale_lock_right) and not self.scale_lock_left:
+                self.scale_lock_right = True
+            self.size[0] = self.size[0] + (sign * anchor_sign * 10)
+            self.prev_x = touch.x
+            changed = True
+        if abs(change_y) >= 9 and not self.move_lock and not self.scale_lock_left and not self.scale_lock_right:
+            if change_y < 0:
+                sign = -1
+            if (touch.y > top or self.scale_lock_top) and not self.scale_lock_bottom:
+                self.scale_lock_top = True
+            elif (touch.y < bottom or self.scale_lock_bottom) and not self.scale_lock_top:
+                self.scale_lock_bottom = True
+                self.pos = (self.pos[0], self.pos[1] + (sign * 10))
+                anchor_sign = -1
+            self.size[1] = self.size[1] + (sign * anchor_sign * 10)
+            self.prev_y = touch.y
+            changed = True
+        return changed
+
+    def on_touch_down(self, touch):
+        x, y = touch.x, touch.y
+        self.prev_x = touch.x
+        self.prev_y = touch.y
+        # if the touch isnt on the widget we do nothing
+        if not self.do_collide_after_children:
+            if not self.collide_point(x, y):
+                return False
+
+        # let the child widgets handle the event if they want
+        touch.push()
+        touch.apply_transform_2d(self.to_local)
+        if super(Scatter, self).on_touch_down(touch):
+            # ensure children don't have to do it themselves
+            if 'multitouch_sim' in touch.profile:
+                touch.multitouch_sim = True
+            touch.pop()
+            self._bring_to_front(touch)
+            return True
+        touch.pop()
+
+        # if our child didn't do anything, and if we don't have any active
+        # interaction control, then don't accept the touch.
+        if not self.do_translation_x and \
+                not self.do_translation_y and \
+                not self.do_rotation and \
+                not self.do_scale:
+            return False
+
+        if self.do_collide_after_children:
+            if not self.collide_point(x, y):
+                return False
+
+        if 'multitouch_sim' in touch.profile:
+            touch.multitouch_sim = True
+        # grab the touch so we get all it later move events for sure
+        self._bring_to_front(touch)
+        touch.grab(self)
+        self._touches.append(touch)
+        self._last_touch_pos[touch] = touch.pos
+
+        return True
 
 if __name__ == '__main__':
 
