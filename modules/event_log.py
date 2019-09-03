1c551d0f04769d00e3e0a41b364237968ed41985
titetil <gtetil@us.tiauto.com>
2019-09-03 14:14:18 -0400

    - Fixed issue where Python3 was adding a blank row to flame logs.

--- modules/event_log.py
+++ modules/event_log.py
@@ -19,7 +19,7 @@ class EventLog:
                     raise
 
     def write(self, data):
-        with open(self.file_path, 'w') as csvfile:
+        with open(self.file_path, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
             writer.writerow(self.header)
             writer.writerow(data)
